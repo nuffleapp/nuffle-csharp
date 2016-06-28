3a8401bc1514b47fd66ded7088fe8ea394fe312d
Andrew Male <amale@zibings.com>
2016-06-28 14:21:04 -0400

    Adding basic d100 roller for automation testing

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine(new Random().Next(0, 100));
+
+			return;
+		}
+	}
+}
